61bb959ecd8d891e3d7bae4602b2f41d48cb215f
alainburindi <alainburindi62@gmail.com>
2020-08-07 23:13:06 +0200

    setup tests

---
+++ tests/schema.py
@@ -0,0 +1,14 @@
+import graphene
+from django.conf import settings
+from graphene_generator.holder import QueriesHolder, MutationsHolder
+
+
+class Query(QueriesHolder, graphene.ObjectType):
+    pass
+
+
+class Mutation(MutationsHolder, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=MutationsHolder)
---
+++ tests/test_al.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+class TestConfig(TestCase):
+
+    def test(self):
+        self.assertEqual(True, True)
